30d2229e26ba3af44f6c662da60b413cc6b800c1
Eric M <eric.merriam@gmail.com>
2018-09-16 09:39:10 -0700

    Added tests

---
+++ tests.rb
@@ -0,0 +1,38 @@
+require 'byebug'
+
+
+
+def merger_first_into_second(arr1, arr2)
+
+	arr2.each_with_index do |item, index|
+		if item == 0
+			half = arr2.length / 2
+			arr2[index] = arr1[index-half]
+		end
+
+	end
+	debugger
+	arr2.sort!
+
+	return arr2
+
+end
+
+arr1 = [1, 3, 5,]
+arr2 = [2, 4, 6, 0, 0, 0]
+
+print merger_first_into_second(arr1, arr2)
+
+
+# def dutch_flag_sort(input_string)
+
+# 	temp_hash = {}
+
+# 	arr = input_string.split('').sort!.reverse!
+
+# 	output = arr.join("")
+# 	return output
+# end
+
+# arr = "GBGGRBRG"
+# print solve(arr)
--- three_sum.rb
+++ three_sum.rb
@@ -6,7 +6,7 @@
 # 	Array is not necessarily sorted.
 # 	Print output as shown. i.e. as strings, one per line, comma separated elements. See sample for clarity.
 # 	Order of elements inside the answer triplets does not matter. i.e. if your output elements are the same, 
-# 	but only in different order, then it's an acceptable solution.
+# 	  but only in different order, then it's an acceptable solution.
 # 	Do not print duplicate triplets. Eg: 1,1,-2 and 1,-2,1 are duplicates.
 # 	If no such triplets are found, then print nothing.
 
@@ -23,14 +23,54 @@
 # 	1 <= N <= 2000
 # 	-10^3 <= element <= 10^3
 # 	Given array may contains duplicate numbers.
+require 'byebug'
 
 def findZeroSum(arr)
 
+	output_array = []
 
+	sorted = arr.sort!
+
+	left_index = 0
+	# [-6, -4, 1, 3, 9, 10]
+	temp_hash = {}
+
+	# Start from Left
+	arr.each_with_index do |value, i|
+		temp_hash[value] = arr[i]
+
+		right_index = sorted.length-1
+		while right_index > 0
+			right_value = arr[right_index]
+			sum = arr[i] + right_value
+			
+			if arr.include?(negate_amount(sum))
+				add_up = []
+				add_up.push(arr[i])
+				add_up.push(right_value)
+				add_up.push(negate_amount(sum))
+				add_up.sort!
+				temp_hash[add_up] = 1
+				print "add_Up is: #{add_up}\n"
+				# output_array.push("#{arr[i]}, #{right_value}, #{negate_amount(sum)}")
+				# output_array[i].push()
+			end
+			debugger
+			right_index -= 1
+		end
+	end
+
+
+# debugger
+	return temp_hash.keys
+end
+
+def negate_amount(input)
+    return -(input.abs)
 end
 
 arr = [10, 3, -4, 1, -6, 9];
-
+print findZeroSum(arr)
 # 10,-4,-6
 # 3,-4,1
 
