15e4e3a911b1f9615bfb276f79325cbddb83cb65
Eric M <eric.merriam@gmail.com>
2018-09-15 17:07:57 -0700

    first commit

---
+++ qsort.rb
@@ -0,0 +1,54 @@
+require 'byebug'
+
+
+def qsort(arr, start, end_)
+	return arr if start >= end_
+
+	initial_pivot = choosePivot(start, end_)
+  pivot_index = partition(arr, start, end_, initial_pivot)
+
+  qsort(arr, start, pivot_index)
+  qsort(arr, pivot_index + 1, end_)
+end
+
+def choosePivot(start, end_)
+	start + (end_ - start)/2; 
+end
+
+def partition(arr, start, end_, pivot_index)
+  pivot_value = arr[pivot_index]
+
+	# Move pivot to end of array
+  arr.swap!(pivot_index, end_ - 1)
+  
+  left_index = start
+  # print "pivot is #{pivot_value}\n"
+  (start...end_ -1).each do |right_index|
+    if arr[right_index] <= pivot_value
+      arr.swap!(right_index, left_index)
+      left_index+= 1
+    end
+  end
+
+  # arr[right_index ] > pivot_value
+  arr.swap!(left_index, end_ - 1)
+  return left_index
+end
+
+class Array
+  def swap!(a,b)
+    self[a], self[b] = self[b], self[a]
+  end
+end
+
+
+arr = [3,1,4,1,5,9,2,6,5,3]
+# arr = [15, 7, 20, 7, 2, 31, 25, 64, 4, 7, 18, 55]
+
+print "#{arr}\n"
+qsort(arr,0,arr.count)
+print "#{arr}\n"
+
+
+
+
