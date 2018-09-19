a3e2527ef070a86e8154a337420cf643843f3699
Eric M <eric.merriam@gmail.com>
2018-09-19 15:08:34 -0700

    update subsets

--- subsets.rb
+++ subsets.rb
@@ -5,17 +5,15 @@ require 'byebug'
 # Print all subsets in a given array
 def parseSubsets(arr)
 	i = 1
-	while i <= arr.length - 1
-		while i <= arr.length
-			no_of_subsets = arr.length/i
-			j = 0
-			while j <= no_of_subsets do
-				this_subset = arr.slice(j..(j + i - 1))
-				print "#{this_subset}\n" if this_subset.length > 0
-				j = j + 1
-			end
-			i = i + 1
+	while i <= arr.length
+		no_of_subsets = arr.length/i
+		j = 0
+		while j <= no_of_subsets do
+			this_subset = arr.slice(j..(j + i - 1))
+			print "#{this_subset}\n" if this_subset.length > 0
+			j = j + 1
 		end
+		i = i + 1
 	end
 
 end
