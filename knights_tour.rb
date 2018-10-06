cf766ee193e147b56b344166d4b8aa591186924f
Eric M <eric.merriam@gmail.com>
2018-10-06 15:35:38 -0600

    knights_tour working but slow

--- knights_tour.rb
+++ knights_tour.rb
@@ -1,40 +1,59 @@
 require 'byebug'
 require './my_graph'
-include MyGraph
-
-
-
 
 def find_minimum_number_of_moves(rows, cols, start_row, start_col, end_row, end_col)
+	seen = []
+	adj_list = []
+	graph = Graph.new(rows, cols)
+debugger
+	start_pos = graph.grid[start_row][start_col]
+	end_pos = graph.grid[end_row][end_col]
 
-	board = set_up(rows, cols)
-
-	start_pos = board[start_row][start_col]
-	end_pos = board[end_row][end_col]
+	return graph_bfs(start_pos, end_pos, graph)
 end
 
-def set_up(rows, cols)
-	i = 0
-	_grid = []
-  0.upto(rows) do |row|
-    _grid[row] = []
-    0.upto(cols) do |col|
-      _grid[row][col] = Vertex.new(i, row, col, nil)
-      i = i + 1
-    end
-  end
-  _grid
+def graph_bfs(start_pos, target_pos, graph)
+	seen = []
+	distance = []
+	seen.push(start_pos)
+	distance[start_pos.label] = 0
+
+	q = Queue.new
+	q.enqueue(start_pos)
+
+	while !q.empty?
+		cur = q.dequeue
+		# print "  +++D #{cur.label}\n"
+		if cur == target_pos
+			return distance[cur.label]
+		end
+
+		cur.possible_moves.each do |nxt|
+			if nxt != nil
+
+				if !seen.include?(nxt)
+					seen.push(nxt)
+					distance[nxt.label] = distance[cur.label] + 1
+					# print "++++E #{nxt.label}\n"
+					q.enqueue(nxt)
+				end
+			end
+		end
+	end
+	return -1
 end
 
 
-rows = 5
-cols = 5
+rows = 2
+cols = 7
 start_row = 0
-start_col = 0
-end_row = 4
+start_col = 5
+end_row = 1
 end_col = 1
 
-find_minimum_number_of_moves(rows, cols, start_row, start_col, end_row, end_col)
+
+
+print find_minimum_number_of_moves(rows, cols, start_row, start_col, end_row, end_col)
 
 
 # Maximum Degree = 8
--- my_graph.rb
+++ my_graph.rb
@@ -1,43 +1,91 @@
-module MyGraph
-  class Vertex
-    attr_accessor :label, :prev, :row, :col
 
-    def initialize(label, row, col, prev)
-    	@label = label
-    	@prev = nil unless !prev.nil?
-    	@row = row
-    	@col = col
+class Vertex < Object
+
+  attr_accessor :label, :prev, :row, :col, :neighbors, :graph
+
+  def initialize(label, row, col, prev, graph)
+  	@label = label
+    @graph = graph
+  	@prev = nil unless !prev.nil?
+  	@row = row
+  	@col = col
+    @neighbors = []
+  end
+
+  def position
+  	[row, col]
+  end
+
+  def valid_position?(_row, _col)
+
+    if _row.between?(0, graph.rows) && _col.between?(0, graph.cols)
+      return true
+    else
+      false
     end
+  end
 
-    def position
-    	[row, col]
-    end
+  def add_edge(adjacent_node)
+    @adjacent_nodes << adjacent_node
+  end
 
-    def valid_position?(row, col, board)
-      if board[row][col].nil?
-        return nil
-      else 
-        return board[row][col]
+
+  def possible_moves
+
+    _add = [[-1,-2],[-2,-1],[-2,1],[-1,2],[1,2],[2,1],[2,-1],[1,-2]]
+    output = []
+    _add.each do |x|
+      a = x[0]
+      b = x[1]
+      if valid_position?(self.row + a, self.col + b)
+        output << graph.grid[self.row + a][self.col + b]
       end
     end
 
-    def possible_moves(board)
-      output = []
-
-      output.push([self.row + 2, self.col - 1]) unless board[self.row + 2, self.col - 1].nil?
-      output.push([self.row + 2, self.col + 1]) unless board[self.row + 2, self.col + 1].nil?
-
-      output.push([self.row - 2, self.col - 1]) unless board[self.row - 2, self.col - 1].nil?
-      output.push([self.row - 2, self.col + 1]) unless board[self.row - 2, self.col + 1].nil?
-
-      output.push([self.row + 1, self.col - 2]) unless board[self.row + 1, self.col - 2].nil?
-      output.push([self.row + 1, self.col + 2]) unless board[self.row + 1, self.col + 2].nil?
-
-      output.push([self.row - 1, self.col - 2]) unless board[self.row - 1, self.col - 2].nil?
-      output.push([self.row - 1, self.col + 2]) unless board[self.row - 1, self.col + 2].nil?
-
-      output
-    end
-
+    output.uniq
   end
 end
+
+class Graph < Array
+  attr_accessor :rows, :cols, :grid
+
+  def initialize(rows, cols)
+    i = 0
+    _grid = []
+
+    (0..rows -1 ).each do |row|
+      _grid[row] = []
+
+      (0..cols - 1).each do |col|
+        _grid[row][col] = Vertex.new(i, row, col, nil, self)
+        print "adding col #{col}\n"
+        i = i + 1
+      end
+    end
+    @grid = _grid
+    @rows = rows - 1
+    @cols = cols - 1
+  end
+end
+
+class Queue < Object
+  def initialize
+    super
+    @data = []
+  end
+
+  def enqueue(element)
+    @data.push(element)
+  end
+
+  def dequeue
+    @data.shift
+  end
+
+  def empty?
+    @data.empty?
+  end
+end
+
+
+
