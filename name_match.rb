a769fddbe7faed50218715a4a176fbdb8cc3d921
Eric M <eric.merriam@gmail.com>
2019-08-11 16:48:23 -0700

    refactored

--- name_match.rb
+++ name_match.rb
@@ -5,7 +5,6 @@ def name_match(aliases, target)
 	aliases.each do |a|
 		a = a.split(' ')
 		return true if !!target.match(/#{a[0]} #{a[2]}/) # keep
-		return true if !!target.match(/#{a[0]} #{a[1]} #{a[2]}/) # keep
 		return true if !!target.match(/#{a[0]} #{a[1][0]} #{a[2]}/) # keep
 		return true if !!target.match(/#{a[0]} #{a[1]}.* #{a[2]}/) # keep
 	end
