df62fa488c54d16985bbf15e3b1ed9c40a5f337e
Eric M <eric.merriam@gmail.com>
2019-08-16 22:08:07 -0700

    Added run length encoder

---
+++ run_length_encoder.rb
@@ -0,0 +1,18 @@
+# Compress a string (only has alphabet characters), with basic encoding,
+ # where you simply count the number of repeated characters. Then also 
+ # write a routine to decompress it.
+
+# Input: "AAAAA" 
+# Output: "5A"
+
+# Input: "BAAAB"
+# Output: "B3AB"
+
+# Input: "ABAB"
+# Output: "ABAB" [We are not concerned about characters repeating in groups]
+
+def RLE(strInput)
+    
+end
+
+print "\n#{RLE("AAAAA")}\n"
