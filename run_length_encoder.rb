0e746fdaadd09f6b5b470459b221ba6ddce1a8ee
Eric M <eric.merriam@gmail.com>
2019-08-16 22:34:06 -0700

    added run length encoder

--- run_length_encoder.rb
+++ run_length_encoder.rb
@@ -10,9 +10,24 @@
 
 # Input: "ABAB"
 # Output: "ABAB" [We are not concerned about characters repeating in groups]
-
+require 'byebug'
 def RLE(strInput)
-    
+	return RLE_helper(strInput, 0, 0)
+end
+
+
+def RLE_helper(strInput, _index, _count)
+	if _index == strInput.length
+		return "#{strInput[_index]}#{_count}"
+	end
+
+	if strInput[_index] == strInput[_index - 1]
+		RLE_helper(strInput, _index + 1, _count + 1)
+	else
+		RLE_helper(strInput, _index, 0)
+	end
+
 end
 
 print "\n#{RLE("AAAAA")}\n"
+# 5A
