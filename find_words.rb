4483a8212b5b472455c94af18ca76e4426da84f6
Eric M <eric.merriam@gmail.com>
2019-08-20 15:21:14 -0700

    pig latinizer added

---
+++ find_words.rb
@@ -0,0 +1,79 @@
+# Given some text (a string with spaces and other characters) and 
+# a bunch of words (list of strings, no spaces in those), 
+# find where each of the words appear in the text.
+
+# For every given word you need to return a list of (zero-based) 
+# indices of where that word starts in the text. If a word isn’t 
+# found in the text, return [-1] for that word.
+
+#
+# Complete the 'find_words' function below.
+#
+# The function accepts STRING and STRING ARRAY as parameter.
+# Return 2D INTEGER ARRAY.
+#
+
+# Build a hash that stores {text.word => char_i}
+
+
+
+require 'byebug'
+
+def find_words(characters, words)
+	pointer_hash = {} 
+
+	output_array = []
+	word_index = 0
+	i = 0
+
+	while i < characters.size
+		if characters[i] == ' ' || characters[i+1] == nil
+
+			if characters[i+1] == nil
+				difference = characters.size - word_index
+			else
+				difference = i - word_index
+			end
+
+ 			word = characters[word_index, difference]
+
+ 			word_index = i + 1
+ 			if pointer_hash.key?(word)
+
+ 				pointer_hash[word].push(word_index)
+ 			else
+ 				pointer_hash[word] = [word_index]
+ 			end
+		end
+		i = i  + 1
+	end
+
+	# lookup the words in the hash
+
+	words.each do |w|
+		if pointer_hash.key?(w)
+			output_array.push(pointer_hash[w])
+		else
+			output_array.push([-1])
+		end
+	end
+
+	return output_array
+
+
+end
+
+
+text = 'you are very very smart'
+words = ['you', 'are', 'very', 'handsome']
+
+print "\n"
+print find_words(text, words)
+print "\n"
+
+# [ [0], [4], [8, 13], [-1] ]
+# “you” is found in the given text starting at the index 0.
+# “are” is found in the given text starting at the index 4.
+# “very” is found in the given text two times, starting at the indices 8 and 13.
+# “handsome” isn’t found in the given text.
+
---
+++ pig_latinizer.rb
@@ -0,0 +1,91 @@
+# Build a pig latin generator. Pig latin is a language made by manipulating
+# english words.
+# Build a function that converts an english word
+# to a pig latin word. The basic rules are:
+# - If the word begins with a consonant, move the first letter to the end
+# of the word and add “ay” to the end. Ex: hello → ellohay
+# - if the word begins with a vowel (a,e,i,o,u), do not move the first
+# letter to the end, simply add “ay” to the end. Ex: apple → appleay
+
+# Build a function which accepts a sentence and returns a pig latin sentence.
+# Ex: hello world → ellohay orldway
+
+
+# Build a pig latin generator. Pig latin is a language made by manipulating
+# english words.
+# Build a function that converts an english word
+# to a pig latin word. The basic rules are:
+# - If the word begins with a consonant, move the first letter to the end
+# of the word and add “ay” to the end. Ex: hello → ellohay
+# - if the word begins with a vowel (a,e,i,o,u), do not move the first
+# letter to the end, simply add “ay” to the end. Ex: apple → appleay
+
+# Build a function which accepts a sentence and returns a pig latin sentence.
+# Ex: hello world → ellohay orldway
+
+# Pig latin words should maintain the case in the letters as submitted
+# to the function (at the same index).
+# Ex: Hello WoRld → Ellohay OrLldway
+
+# Ex: The anteater hAs eaten some ants
+# Ex: Hetay anteateray aShay eatenay omesay antsay
+
+def pig_latinizer_sentence(sentence)
+  sentence_array = sentence.split(' ')
+  output_array = []
+  sentence_array.each do |word|
+    output_array.push(pig_latinizer(word))
+  end
+  output_array.join(' ')
+end
+
+
+def pig_latinizer(input_string)
+  vowels = ["a","e","i","o","u"]
+
+  first_letter = input_string[0]
+  input_array = input_string.split('')
+  return_val = ''
+
+  upcases = find_upcase(input_string)
+
+  if !vowels.include?(first_letter.downcase)
+    first_letter = input_array.shift.downcase
+    return_val = "#{input_array.join}#{first_letter}ay"
+    return_val
+  else
+    return_val = "#{input_array.join}ay"
+    return_val
+  end
+
+  return_val = return_val.downcase
+
+  if upcases.length > 0
+    upcases.each do |u|
+      return_val[u] = return_val[u].upcase
+    end
+  end
+  return return_val
+
+end
+
+def find_upcase(input_string)
+  i = 0
+  return_vals = []
+  while i < input_string.length
+    if !!/[[:upper:]]/.match(input_string[i])
+      return_vals.push(i)
+      input_string[i].downcase
+    end
+  
+    i = i + 1
+  end
+  return return_vals
+
+end
+
+
+
+puts "\n"
+puts pig_latinizer_sentence("The anteaTer hAs eaten some ants")
+puts "\n"
