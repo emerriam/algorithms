5ab4796daa354ff9257283209f5d4eec04d3543b
Eric M <eric.merriam@gmail.com>
2019-08-25 15:37:36 -0700

    Added four_billion

---
+++ four_billion.rb
@@ -0,0 +1,24 @@
+def find_integer(arr)
+
+	arr_length = arr.shift
+	arr.sort!
+
+	arr.each do |val|
+		return find_integer_in_array(arr, val)
+	end
+end
+
+def find_integer_in_array(arr, target)
+
+  i = 1
+	while i < 4000000
+		if !arr.include?(i)
+			return i
+		end
+		i = i + 1
+	end
+end
+
+
+arr = [4, 0, 1, 2, 3]
+puts "\n#{find_integer(arr)}\n"
